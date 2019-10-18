b56fb3bbb89193819066c888f62672c067b2598a
Lee Hicks <lee@truss.works>
2019-10-18 21:46:11 +0000

    Pushing to slack messages

--- sleuth/sleuth.py
+++ sleuth/sleuth.py
@@ -1,10 +1,12 @@
 import datetime as dt
+import json
 import logging
 
 
 import boto3
 from pythonjsonlogger import jsonlogger
 from tabulate import tabulate
+import requests
 
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
@@ -35,6 +37,11 @@ logHandler.setFormatter(formatter)
 LOGGER.addHandler(logHandler)
 
 IAM = boto3.client('iam')
+SSM = boto3.client('ssm')
+
+SLACK_USERS = {
+    'lee': 'UP6A26UAE'
+}
 
 def get_iam_key_info(user):
     """Fetches User key info
@@ -139,17 +146,113 @@ def print_key_report(users, status_filter=None):
 
     print(tabulate(tbl_data, headers=['UserName', 'Key ID', 'Status', 'Expires in Days']))
 
-# pull slack users
-
-# match IAM user to slack user
 
 # push notification to integration with list of upcoming
 
 # if user is past threshold disable key
 
+def get_ssm_value(ssm_path):
+    """Get SSM Parameter value
+
+    Parameters:
+    ssm_path (str): Path of the parametere to return
+
+    Returns:
+    str: Value of parameter
+    """
+    resp = SSM.get_parameter(Name=ssm_path,
+                             WithDecryption=True)
+    return resp['Parameter']['Value']
+
+
+def send_slack_message(payload):
+    webhook = get_ssm_value('/integrations/test_general')
+
+    resp = requests.post(webhook, data=json.dumps(payload),
+                         headers={'Content-Type': 'application/json'})
+
+    if resp.status_code == requests.codes.ok:
+        LOGGER.info('Successfully posted to slack')
+    else:
+        msg = 'Unsuccessfully posted to slack, response {}, {}'.format(resp.status, resp.code)
+        LOGGER.error(msg)
+
+
+def find_user(username):
+    """Helper func to return a mention if user matches
+    """
+
+    norm_username = username.lower()
+    if username in SLACK_USERS:
+        return '<@{}>'.format(SLACK_USERS[username])
+    else:
+        return username
+
+
+def prepare_message(old_users, expired_users):
+    """Prepares message for sending via webhook
+
+    Parameters:
+    old_users (list): Dicts containing user and key age
+    expired_users (list): Dicts containing user and key age
+
+    Returns:
+    None
+    """
+
+    old_attachment = {
+        'title': 'IAM users with access keys expiring',
+        'color': '#ffff00', #yellow
+        'fields': [
+            {
+                'title': 'Users',
+                'value': '\n'.join(['{}\'s key expires in {} days'.format(find_user(u['UserName']), u['ExpiresIn'])for u in old_users]),
+            }
+        ]
+    }
+
+    expired_attachment = {
+        'title': 'IAM users with disabled access keys',
+        'color': '#ff0000', #red
+        'fields': [
+            {
+                'title': 'Users',
+                'value': '\n'.join(['{}\'s key is disabled'.format(find_user(u['UserName'])) for u in expired_users]),
+            }
+        ]
+    }
+
+    howto_attachment = {
+        'title': 'Access Key Rotation Instructions',
+        'text': 'https://github.com/transcom/ppp-infra/tree/master/transcom-ppp#rotating-aws-access-keys'
+    }
+
+
+    # include master one
+    msg = {
+        # "text": "IAM Key Updates",
+        "attachments": []
+    }
+
+    # only add the attachments that have users
+    if len(old_users) > 0:
+        msg['attachments'].append(old_attachment)
+
+    if len(expired_users) > 0:
+        msg['attachments'].append(expired_attachment)
+
+
+
+    # only post to slack if needed
+    if len(msg['attachments']) > 0:
+        msg['attachments'].append(howto_attachment)
+        send_slack_message(msg)
+    else:
+        LOGGER.info('Nothing to report')
+
 
 def audit():
-    LOGGER.info('Sleuth running', extra={'lalala':'blahblah'})
+    LOGGER.info('Sleuth running')
     iam_users = get_iam_users()
     for u in iam_users:
         for k in u['keys']:
@@ -162,18 +265,31 @@ def audit():
     #mainly for debugging
     print_key_report(iam_users)
 
+    old_keys_info = []
+    expired_keys_info = []
     # lets take actions on keys
-    # first we'll notify on old keys
-    # second we'll notify and disable expired keys
     for u in iam_users:
         for k in u['keys']:
             if k['Valid'] == 'old':
                 LOGGER.info('User {} has an old key'.format(u['UserName']))
+                old_keys_info.append({
+                    'UserName': u['UserName'],
+                    'AccessKeyId': k['AccessKeyId'],
+                    'Valid': k['Valid'],
+                    'ExpiresIn': k['ExpiresIn']
+                })
 
             if k['Valid'] == 'expire':
                 LOGGER.info('User {} key will be disabled'.format(u['UserName']))
                 disable_key(u['UserName'], k['AccessKeyId'])
+                expired_keys_info.append({
+                    'UserName': u['UserName'],
+                    'AccessKeyId': k['AccessKeyId'],
+                    'Valid': k['Valid'],
+                    'ExpiresIn': k['ExpiresIn']
+                })
 
+    slack_msg = prepare_message(old_keys_info, expired_keys_info)
 
 
 if __name__ == '__main__':
