9504a484b49befe816ff8fe0a693be9707980a2d
ralren <ren@truss.works>
2020-05-11 11:21:49 -0700

    Remove variable enable_sns_topic for now

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -2,7 +2,10 @@ resource "aws_sns_topic" "slack_events" {
   name = "slack-events-topic"
 }
 
-module iam_sleuth {
-  source        = "../.."
-  sns_topic_arn = aws_sns_topic.slack_events.arn
+module "iam_sleuth_with_sns_topic" {
+  source = "../.."
+
+  sns_topic_arn  = aws_sns_topic.slack_events.arn
+  expiration_age = 120
+  warning_age    = 10
 }
