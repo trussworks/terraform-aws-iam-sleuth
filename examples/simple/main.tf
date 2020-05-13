5833e792094e7cd967360813800b9aab1c7eeac7
ralren <ren@truss.works>
2020-05-13 13:11:56 -0700

    Get rid of ssm parameter since we can just add a fake slack webhook url

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -2,13 +2,6 @@ resource "aws_sns_topic" "slack_events" {
   name = "slack-events-topic"
 }
 
-resource "aws_ssm_parameter" "slack_url" {
-  name        = "/iam-sleuth/slack-url"
-  description = "Slack webhook url to send messages directly through"
-  type        = "SecureString"
-  value       = "fake slack webhook url"
-}
-
 module "iam_sleuth_with_sns_topic_and_slack_url" {
   source = "../.."
 
@@ -19,5 +12,5 @@ module "iam_sleuth_with_sns_topic_and_slack_url" {
   sns_message         = "This will show whose keys need to be rotated."
   slack_message_title = "IAM Key Report"
   slack_message_text  = "Who needs their keys rotated?"
-  slack_url           = aws_ssm_parameter.slack_url.value
+  slack_url           = "fake slack webhook url"
 }
